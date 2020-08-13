0cd2e9623d35cc50e48962d3eff57a9ef1d9962c
flaviojasilva <65636630+flaviojasilva@users.noreply.github.com>
2020-08-13 06:46:59 -0300

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,28 @@
 import './App.css'
 import React from 'react';
 
+import Familia from './components/basic/Familia'
+import FamiliaMembro from './components/basic/FamiliaMembro'
 import Primeiro from './components/basic/Primeiro';
 import ComParametro from './components/basic/Comparametro';
 import Fragmento from './components/basic/Fragmento';
 import Aleatorio from './components/basic/Aleatorio';
 import Card from './components/layout/Card'
 
+
 export default () =>
     <div className='App'>
         <h1>Fundamentos React</h1>
-        
+
         <div className="Cards">
+            <Card titulo='#05 - Componente com Filhos' color="#3CB371">
+                <Familia sobrenome='Rêgo'>
+                    <FamiliaMembro nome='Gustavo'/>
+                    <FamiliaMembro nome='Matrin'/>
+                    <FamiliaMembro nome='Ana'/>
+                </Familia>
+            </Card>
+
             <Card titulo='#04 - Valor Aleatório' color="#6A5ACD">
                 <Aleatorio
                     min={1}
@@ -23,7 +34,7 @@ export default () =>
             </Card>
 
             <Card titulo='#02 - Com Parâmetro' color="#008B8B">
-                <ComParametro 
+                <ComParametro
                     titulo='Situação do aluno'
                     aluno='Marcos'
                     nota={6.9}
