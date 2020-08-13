bf9f149b7c3dc5d775478750d2b42b24413f9fae
flaviojasilva <65636630+flaviojasilva@users.noreply.github.com>
2020-08-13 05:40:16 -0300

    update

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react';
 
 import Primeiro from './components/basic/Primeiro';
@@ -7,27 +8,30 @@ import Aleatorio from './components/basic/Aleatorio';
 import Card from './components/layout/Card'
 
 export default () =>
-    <div id='app'>
+    <div className='App'>
         <h1>Fundamentos React</h1>
-        <Card titulo = '#04 - Valor Aleatório'>
-            <Aleatorio 
-                min = {1}
-                max = {60}/>
-        </Card>
-
-        <Card titulo = '#03 - Fragmento'>
-            <Fragmento/> 
-        </Card>
-
-        <Card titulo = '#02 - Com Parâmetro'>
-            <ComParametro
-                titulo='Situação do aluno'
-                aluno='Marcos'
-                nota={6.9}
-            />
-        </Card>
         
-        <Card titulo = '#01 - Primeiro componente'>
-            <Primeiro/>
-        </Card>
+        <div className="Cards">
+            <Card titulo='#04 - Valor Aleatório' color="#6A5ACD">
+                <Aleatorio
+                    min={1}
+                    max={60} />
+            </Card>
+
+            <Card titulo='#03 - Fragmento' color="#00CED1">
+                <Fragmento />
+            </Card>
+
+            <Card titulo='#02 - Com Parâmetro' color="#008B8B">
+                <ComParametro 
+                    titulo='Situação do aluno'
+                    aluno='Marcos'
+                    nota={6.9}
+                />
+            </Card>
+
+            <Card titulo='#01 - Primeiro componente' color="#F08080">
+                <Primeiro />
+            </Card>
+        </div>
     </div>
